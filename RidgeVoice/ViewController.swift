117220c10b8b6f1a431aedf8d7ada6fe8ff1e0ab
amit <amathurn@in.ibm.com>
2019-07-09 14:48:23 +0530

    Initial Project Setup.

---
+++ RidgeVoice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RidgeVoice
+//
+//  Created by Amit Mathur on 7/9/19.
+//  Copyright © 2019 Amit Mathur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
